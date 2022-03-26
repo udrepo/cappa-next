360e248272dcc630af3524786a0167caeaccf8f0
ulan <ulan.0418@gmail.com>
2022-03-26 23:36:01 +0600

    index styling and added logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,3 @@
-
 import Link from 'next/link'
 import Image from 'next/image'
 import useTranslation from "next-translate/useTranslation";
@@ -14,21 +13,25 @@ export default function NavBar() {
 
 
     const setHeight = () => {
-        return 67;
+        if (size.width < 1000) return 52;
+        return 63;
     }
 
     const setWidth = () => {
-       return 263
+        if (size.width < 1000) return 181;
+        return 220;
     }
 
     console.log(t('common:nav'));
-    return <nav className="m-4 my-2 md:mx-20 md:my-4 flex justify-between">
-        <div><Link href="/"><a className="text-dark-blue text-2xl font-bold content-center">
+    return <nav className="m-4 my-0 md:mx-20 md:my-4 flex justify-between">
+        <div className="self-end"><Link href="/"><a className="">
             <Image src="/assets/logo.png" width={setWidth()} height={setHeight()}/>
         </a></Link></div>
         <div className="flex py-4">
-            <div className="hover:text-whatsapp text-dark-blue hover:text-whatsapp hover:border-b-2 border-dark-blue"><Link href="/"><a><FaWhatsapp size="30"/></a></Link></div>
-            <div className="ml-4 hover:text-light-blue hover:border-b-2 border-dark-blue"><Link href="/"><a><FaQuestionCircle size="30"/></a></Link></div>
+            <div className="hover:text-whatsapp text-dark-blue hover:text-whatsapp hover:border-b-2 border-dark-blue">
+                <Link href="/"><a><FaWhatsapp size="30"/></a></Link></div>
+            <div className="ml-4 hover:text-light-blue hover:border-b-2 border-dark-blue"><Link
+                href="/"><a><FaQuestionCircle size="30"/></a></Link></div>
         </div>
     </nav>
 }
